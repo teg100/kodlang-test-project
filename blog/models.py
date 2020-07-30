5a4b1a73cddb40fcb6e9e92f201e4e62dad0dd98
Vladimir Chuikov <python46dev@gmail.com>
2020-07-30 22:15:00 +0400

    Добавляет дату создания и дату модификации для Post, отображение этих полей в шаблон

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,8 @@ class Post(models.Model):
     title = models.CharField(max_length=256)
     image = models.ImageField(upload_to="images/")
     text = models.TextField()
+    date_create = models.DateTimeField(auto_now_add=True)
+    date_modify = models.DateTimeField(auto_now=True)
 
 
     def __str__(self):
