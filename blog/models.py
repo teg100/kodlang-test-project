151935e7eb5175d2698efc2527ac6e4d823f45ee
Vladimir Chuikov <python46dev@gmail.com>
2020-07-30 22:27:55 +0400

    Добавляет переход к просмотру поста.

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.shortcuts import reverse
 # Create your models here.
 
 class Post(models.Model):
@@ -9,6 +9,8 @@ class Post(models.Model):
     date_create = models.DateTimeField(auto_now_add=True)
     date_modify = models.DateTimeField(auto_now=True)
 
+    def get_absolute_url(self):
+        return reverse('post_detail_url', kwargs={'pk': self.id})
 
     def __str__(self):
         return self.title
