0c168342de2b4dcdbdf4ff2f814e093b1103f1f4
Vladimir Chuikov <python46dev@gmail.com>
2020-07-30 22:32:56 +0400

    Добавляет сортировку постов по дате публикации от свежих, к страрым. Добавляет индекс в бд для title, text.

--- blog/models.py
+++ blog/models.py
@@ -3,15 +3,16 @@ from django.shortcuts import reverse
 # Create your models here.
 
 class Post(models.Model):
-    title = models.CharField(max_length=256)
+    title = models.CharField(max_length=256, db_index=True)
     image = models.ImageField(upload_to="images/")
-    text = models.TextField()
+    text = models.TextField(db_index=True)
     date_create = models.DateTimeField(auto_now_add=True)
-    date_modify = models.DateTimeField(auto_now=True)
 
     def get_absolute_url(self):
         return reverse('post_detail_url', kwargs={'pk': self.id})
 
     def __str__(self):
         return self.title
+    class Meta:
+        ordering = ['-date_create']
 
