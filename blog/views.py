9e47d6ff3298caa180b19c732aab96f5320183ca
Vladimir Chuikov <python46dev@gmail.com>
2020-07-31 19:10:28 +0400

    Добавляет функции выборки.

--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,15 @@ class PostsListView(ListView):
     template_name = 'blog/posts_list.html'
     context_object_name = 'posts'
 
+    def get_queryset(self):
+        return Post.objects.all()[1:10]
+
+    def get_context_data(self,  **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['last_post'] = Post.objects.first()
+        return context
+
+
 class PostView(DetailView):
     model = Post
     template_name = 'blog/post.html'
