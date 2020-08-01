09d87c0868337bff48a0f264d48517ae23cd6218
Vladimir Chuikov <python46dev@gmail.com>
2020-08-01 17:46:14 +0400

    Fix deploy heroku

--- kodlang_test/settings.py
+++ kodlang_test/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-hcz7u$$r^dd4rz+lei(&bg(1dsc$q^tx*75-j@n!atphng5^$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- kodlang_test/wsgi.py
+++ kodlang_test/wsgi.py
@@ -14,6 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kodlang_test.settings')
 
 application = get_wsgi_application()
-
-from whitenoise import WhiteNoise
-application = WhiteNoise(application)
