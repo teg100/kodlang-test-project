4c6d92b36809f14b9116ca2843cb7337bea5d9d5
Vladimir Chuikov <python46dev@gmail.com>
2020-08-01 17:40:57 +0400

    Fix deploy

--- kodlang_test/settings.py
+++ kodlang_test/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
